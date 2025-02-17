84d0032fd9c225e8de4ca46877087f7fccb61219
iste19 <istefatsawdainan@gmail.com>
2025-02-17 23:17:54 +1300

    getting the modals working (will refactor later)

---
+++ scripts/experienceTimelineData.js
@@ -0,0 +1,59 @@
+const timelineData = [
+  {
+    date: "Nov 2024 - Feb 2025",
+    role: "Software Development Engineer",
+    company: "Pushpay",
+    type: "Internship",
+    modalContent: {
+      jobDescription: ["blabla", "blabla"],
+      images: [null],
+    },
+    side: "left"
+  },
+  {
+    date: "Jan 2023 - June 2024",
+    role: "Assistant & Marker for Statistics Dept.",
+    company: "The University of Auckland",
+    type: "Part-time",
+    modalContent: {
+      jobDescription: ["blabla", "blabla"],
+      images: [null],
+    },
+    side: "right"
+  },
+  {
+    date: "July 2023 - Sep 2023",
+    role: "Credit Consultant",
+    company: "Heartland Bank LTD",
+    type: "Contract",
+    modalContent: {
+      jobDescription: ["blabla", "blabla"],
+      images: [null],
+    },
+    side: "left"
+  },
+  {
+    date: "Nov 2022 - Jan 2023",
+    role: "Customer Service Representative",
+    company: "Bachcare, The Holiday Home Company",
+    type: "Full-time",
+    modalContent: {
+      jobDescription: ["blabla", "blabla"],
+      images: [null],
+    },
+    side: "right"
+  },
+  {
+    date: "Jan 2020 - Jan 2023",
+    role: "Customer Service Representative",
+    company: "Sky TV",
+    type: "Part-time",
+    modalContent: {
+      jobDescription: ["blabla", "blabla"],
+      images: [null],
+    },
+    side: "left"
+  },
+];
+
+export default timelineData;
--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,71 @@
-const toggle = document.getElementById("darkModeToggle")
+import timelineData from "./experienceTimelineData.js";
+
+const toggle = document.getElementById("darkModeToggle");
 
 toggle.addEventListener("change", () => {
-    toggle.checked ? document.body.classList.add("dark-mode") : document.body.classList.remove("dark-mode")
-})
+  toggle.checked
+    ? document.body.classList.add("dark-mode")
+    : document.body.classList.remove("dark-mode");
+});
 
+//console.log(timelineData);
 
+const timeline = document.getElementById("timeline");
 
+function loadTimeline() {
+  timeline.innerHTML = "";
 
-// Get the modal
-var modal = document.getElementById("myModal");
+  timelineData.forEach((item, index) => {
+    let modalContent = "";
 
-// Get the button that opens the modal
-var btn = document.getElementById("pushpayCard");
+    modalContent = `
+    <div id="modal-${index}" class="modal">
+      <div class="modal-content">
+        <span class="close" id="close-${index}">&times;</span>
+        <ul>
+        ${item.modalContent.jobDescription
+          .map((point) => `<li>${point}</li>`)
+          .join("")}
+        </ul>
+      </div>
+    </div>
+    `;
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    const timelineItem = `
+      <div class="timeline-item ${item.side}">
+        <div class="timeline-content" id="timeline-content-${index}">
+          <h3>${item.date}</h3>
+          <div><span class="role">${item.role}</span> · ${item.type}</div>
+          <p><em>${item.company}</em></p>
+        </div>
+        ${modalContent}
+      </div>
+    `;
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+    timeline.innerHTML += timelineItem;
+
+    document
+      .getElementById(`timeline-content-${index}`)
+      .addEventListener("click", () => {
+        document.getElementById(`modal-${index}`).style.display = "block";
+      });
+
+    document.getElementById(`close-${index}`).addEventListener("click", () => {
+      document.getElementById(`modal-${index}`).style.display = "none";
+    });
+  });
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+loadTimeline();
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+for (let i = 0; i < timelineData.length; i++) {
+  document
+    .getElementById(`timeline-content-${i}`)
+    .addEventListener("click", () => {
+      document.getElementById(`modal-${i}`).style.display = "block";
+    });
+
+  document.getElementById(`close-${i}`).addEventListener("click", () => {
+    document.getElementById(`modal-${i}`).style.display = "none";
+  });
 }
