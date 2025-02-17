2306d6b87377721584a5bdd5cd8ac551fd2d1a2d
iste19 <istefatsawdainan@gmail.com>
2025-02-17 19:41:47 +1300

    modal experiment from docs (static data)

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,32 @@ const toggle = document.getElementById("darkModeToggle")
 toggle.addEventListener("change", () => {
     toggle.checked ? document.body.classList.add("dark-mode") : document.body.classList.remove("dark-mode")
 })
+
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("pushpayCard");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
