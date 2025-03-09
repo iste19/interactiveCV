7f75b0e80dfd2a408e21f4a7ffc66df90a6c8c38
iste19 <istefatsawdainan@gmail.com>
2025-03-09 15:34:52 +1300

    basic auth routes without functionality

--- cv-backend/server.js
+++ cv-backend/server.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api/comments", require("./routes/commentRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
 
 app.listen(port, () => {
